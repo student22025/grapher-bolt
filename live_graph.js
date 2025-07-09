259a558e96983b9bc845b10a9b58ab80cb3bf7aa
student22025 <studentmail22025@gmail.com>
2025-07-09 11:07:21 +0530

    Updated live_graph.js

--- live_graph.js
+++ live_graph.js
@@ -818,11 +818,12 @@ export class LiveGraph {
           this.hideUploadProgress();
           console.error('Upload failed:', error);
           this.showErrorMessage('Upload failed: ' + error.message);
-      } else if (this.data.length === 0) {
+              } else if (this.data.length === 0) {
         this.showErrorMessage('No data to save - recording was empty');
       } else {
         this.showErrorMessage('No Google Drive link set - data not uploaded');
       }
+          }
     }
     
     this.renderSidebar();
